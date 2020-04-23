6865a751a7ce4d6f16b2a3643cb724c2a4b32637
Rondel Hallare <hallarerondel@yahoo.com>
2020-04-23 13:16:15 +0800

    Create tournament-feature.component.ts

---
+++ src/app/tournament-feature/tournament-feature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-feature',
+  templateUrl: './tournament-feature.component.html',
+  styleUrls: ['./tournament-feature.component.less']
+})
+export class TournamentFeatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
