2c54aaf7535c99cc4943c2341354399c2ffae9cf
Haniel023 <54757754+Haniel023@users.noreply.github.com>
2020-05-08 22:10:05 +0800

    add user profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { UpdatePageComponent } from './announcement/update-page/update-page.comp
 import { ContactComponent } from './contact/contact.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { PickerModule } from '@ctrl/ngx-emoji-mart';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 
@@ -68,6 +69,7 @@ import { PickerModule } from '@ctrl/ngx-emoji-mart';
         TournamentFooterComponent,
         UpdatePageComponent,
         ContactComponent,
+        UserProfileComponent,
 
 
        
