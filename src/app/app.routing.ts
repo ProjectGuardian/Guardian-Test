0029b849f9a3277120d40d7a412cebc14c908225
CjEspinoza <espinoza.cj3@gmail.com>
2020-05-08 10:14:51 +0800

    reformat image

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register';
+import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './_helpers';
 import { AdminComponent } from './admin/admin.component';
 import { TournamentHomeComponent } from './tournament/tournament-home/tournament-home.component';
@@ -12,9 +12,9 @@ const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'Admin', component: AdminComponent},
-    { path: 'tournament-home', component: TournamentHomeComponent},
-    { path: 'update-page', component: UpdatePageComponent},
+    { path: 'Admin', component: AdminComponent, canActivate: [AuthGuard]},
+    { path: 'tournament-home', component: TournamentHomeComponent, canActivate: [AuthGuard]},
+    { path: 'update-page', component: UpdatePageComponent, canActivate: [AuthGuard]},
 
     { path: '**', redirectTo: '' }
 ];
