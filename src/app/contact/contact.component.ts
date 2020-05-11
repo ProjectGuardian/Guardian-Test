04257c402061c411ee3f59e99dde3730d017bcb2
CjEspinoza <espinoza.cj3@gmail.com>
2020-05-11 10:12:01 +0800

    add functions to contact footer

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
-
+  home(){
+    this.router.navigate(['/home']);
+  }
+  tournamentPage(){
+    this.router.navigate(['/tournament-home']);
+}
+  blogPage(){
+    this.router.navigate(['/update-page']);
+  }
 }
