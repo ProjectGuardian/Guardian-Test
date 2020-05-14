7d8c9a90e72b6ddb112334b51d38db116bbdbbeb
Haniel023 <54757754+Haniel023@users.noreply.github.com>
2020-05-14 22:36:22 +0800

    add function on side bar

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
@@ -16,6 +17,17 @@ export class SideNavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  constructor(private breakpointObserver: BreakpointObserver,private router: Router) {}
+  home(){
+    this.router.navigate(['/home']);
+}
+  tournamentPage(){
+    this.router.navigate(['/tournament-home']);
+}
+  blogPage(){
+    this.router.navigate(['/update-page']);
+}
+  contactPage(){
+    this.router.navigate(['/contact']);
+}
 }
