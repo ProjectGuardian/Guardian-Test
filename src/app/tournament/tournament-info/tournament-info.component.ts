6e4f0974c32c4ddf448695bc2d826f5bd78d214a
CjEspinoza <espinoza.cj3@gmail.com>
2020-05-14 22:58:55 +0800

    add cms to bracket

--- src/app/tournament/tournament-info/tournament-info.component.ts
+++ src/app/tournament/tournament-info/tournament-info.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ItemService } from '@/_services/item.service';
+import { Sched, Vid, Brackets, Marqs } from '@/_models/item';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tournament-info',
   templateUrl: './tournament-info.component.html',
   styleUrls: ['./tournament-info.component.less']
 })
 export class TournamentInfoComponent implements OnInit {
-
-  constructor() { }
+  brackets: Brackets[];
+  bracket: Brackets={
+    id:'',
+    bLink:''
+  }
+  constructor(private router : Router, private itemService: ItemService) { }
 
   ngOnInit(): void {
+    this.itemService.getBracket().subscribe(brackets=>{
+      this.brackets = brackets;
+    })
   }
-
+  deleteBracket(event, bracket: Brackets){
+    this.itemService.deleteBracket(bracket);
+  }
 }
