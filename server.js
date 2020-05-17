9df765073309abe2fe50c04e53c516235e6b78f9
CjEspinoza <espinoza.cj3@gmail.com>
2020-05-17 18:18:37 +0800

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+var app_path = './dist/Guardian-Test';
 
 app.use(express.static('./dist/guardian-test'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/guardian-test/'}),
-);
-
-app.listen(process.env.PORT || 8080);
+app.use('/', express.static(path.join(__dirname, app_path)))
+.get('*',(req, res)=> res.sendFile(path.join(__dirname, app_path + '/index.html')))
+.listen(PORT, ()=> console.log(`Listening on ${PORT}`));
