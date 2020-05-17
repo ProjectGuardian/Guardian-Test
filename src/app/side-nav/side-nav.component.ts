321055eca9a8beba434d0fcd29b1fdf89161b73e
CjEspinoza <espinoza.cj3@gmail.com>
2020-05-17 21:57:06 +0800

    edit side-nav ts

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -24,7 +24,6 @@ export class SideNavComponent {
     document.getElementById('tournament').style.backgroundColor = "transparent";
     document.getElementById('updates').style.backgroundColor = "transparent";
     document.getElementById('contacts').style.backgroundColor = "transparent";
-    document.getElementById('admin').style.backgroundColor = "transparent";
 }
   tournamentPage(){
     this.router.navigate(['/tournament-home']);
@@ -32,7 +31,6 @@ export class SideNavComponent {
     document.getElementById('Home').style.backgroundColor = "transparent";
     document.getElementById('updates').style.backgroundColor = "transparent";
     document.getElementById('contacts').style.backgroundColor = "transparent";
-    document.getElementById('admin').style.backgroundColor = "transparent";
 }
   blogPage(){
     this.router.navigate(['/update-page']);
@@ -40,7 +38,6 @@ export class SideNavComponent {
     document.getElementById('tournament').style.backgroundColor = "transparent";
     document.getElementById('Home').style.backgroundColor = "transparent";
     document.getElementById('contacts').style.backgroundColor = "transparent";
-    document.getElementById('admin').style.backgroundColor = "transparent";
 }
   contactPage(){
     this.router.navigate(['/contact']);
@@ -48,7 +45,6 @@ export class SideNavComponent {
     document.getElementById('tournament').style.backgroundColor = "transparent";
     document.getElementById('updates').style.backgroundColor = "transparent";
     document.getElementById('Home').style.backgroundColor = "transparent";
-    document.getElementById('admin').style.backgroundColor = "transparent";
 }
   dashboard(){
     this.router.navigate(['/Admin']);
