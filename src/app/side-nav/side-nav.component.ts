d92c620a0da4e6b498bbf9e412085553d99b3a0f
Josiah Autor <autorjosiah@gmail.com>
2020-05-17 12:09:11 +0800

    Add active on page click in side-nav component

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -21,15 +21,31 @@ export class SideNavComponent {
   constructor(private breakpointObserver: BreakpointObserver,public auth: AuthenticationService,private router: Router) {}
   home(){
     this.router.navigate(['/home']);
+    document.getElementById('Home').style.backgroundColor = "rgba(190, 190, 190, 0.116)";
+    document.getElementById('tournament').style.backgroundColor = "transparent";
+    document.getElementById('updates').style.backgroundColor = "transparent";
+    document.getElementById('contacts').style.backgroundColor = "transparent";
 }
   tournamentPage(){
     this.router.navigate(['/tournament-home']);
+    document.getElementById('tournament').style.backgroundColor = "rgba(190, 190, 190, 0.116)";
+    document.getElementById('Home').style.backgroundColor = "transparent";
+    document.getElementById('updates').style.backgroundColor = "transparent";
+    document.getElementById('contacts').style.backgroundColor = "transparent";
 }
   blogPage(){
     this.router.navigate(['/update-page']);
+    document.getElementById('updates').style.backgroundColor = "rgba(190, 190, 190, 0.116)";
+    document.getElementById('tournament').style.backgroundColor = "transparent";
+    document.getElementById('Home').style.backgroundColor = "transparent";
+    document.getElementById('contacts').style.backgroundColor = "transparent";
 }
   contactPage(){
     this.router.navigate(['/contact']);
+    document.getElementById('contacts').style.backgroundColor = "rgba(190, 190, 190, 0.116)";
+    document.getElementById('tournament').style.backgroundColor = "transparent";
+    document.getElementById('updates').style.backgroundColor = "transparent";
+    document.getElementById('Home').style.backgroundColor = "transparent";
 }
   logout() {
   this.auth.logout();
