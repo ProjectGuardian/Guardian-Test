a51252049cddc4e5e77d12cadc9403ebe11b6c38
ProjectGuardian <57053792+ProjectGuardian@users.noreply.github.com>
2021-02-08 18:45:20 +0800

    change firebase url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,11 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyDxip5hksnsEkU45ZEbNvtGg7yWEdkYhzI",
-    authDomain: "fs1prod-662df.firebaseapp.com",
-    databaseURL: "https://fs1prod-662df.firebaseio.com",
-    projectId: "fs1prod-662df",
-    storageBucket: "fs1prod-662df.appspot.com",
-    messagingSenderId: "161456096530",
-    appId: "1:161456096530:web:5932efbd0f96e3e3d01fcb"
+    apiKey: "AIzaSyBSzxmEtN-ZcW_pxZo4jc8iIHHqILL8LXE",
+    authDomain: "guardianv3-27ab5.firebaseapp.com",
+    projectId: "guardianv3-27ab5",
+    storageBucket: "guardianv3-27ab5.appspot.com",
+    messagingSenderId: "417978674684"
   }
 }
 
